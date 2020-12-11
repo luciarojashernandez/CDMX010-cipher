7f42d3a94a6eeb85297bc82810348b89c09a03ca
LU <rojaslucia.h@gmail.com>
2020-12-10 18:05:46 -0600

    // Acomodé interfaz en un sólo archivo html

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,21 @@
-const cipher = {
-  // ...
+/*const cipher = {
+  encode: function() {
+    let text = 'adriana';
+    for(let i = 0; i < text.length; i++){
+      console.log(text[i]);
+    }
+  },
+  decode
 };
 
-export default cipher;
+export default cipher;*/
+
+/*const cipher = {
+  function encode13(texto) {
+    for (let i = 0; i < textp.length; i ++)
+    console.log (texto[i]);
+    
+  }
+}
+
+encode13 ("A")*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import cipher from './cipher.js';
+/*import cipher from './cipher.js';
 
-console.log(cipher);
+console.log(cipher);*/
+
+const cipher = {
+    function encode13(texto) {
+      for (let i = 0; i < textp.length; i ++)
+      console.log (texto[i]);
+      
+    }
+  }
+  
+  encode13 ("A")

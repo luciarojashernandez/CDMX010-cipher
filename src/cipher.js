17105439d38e509ac623ddddc262d28bcd63e7ea
LU <rojaslucia.h@gmail.com>
2020-12-21 12:40:33 -0600

    Cambio css, cambios html(textarea)

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,43 @@
-// const cipher = {
-//   encode: function() {
-//     let text = 'adriana';
-//     for(let i = 0; i < text.length; i++){
-//       console.log(text[i]);
-//     }
-//   },
-//   decode
-// };
 
-// export default cipher;
-
-const encode = (string, offset)=>{
-    let result = "", code;
+const  encode = (string, desplazamiento)=>{
+  let result = "", code;
     for (let i=0; i<string.length;i++){
       if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-        code = (((string.charCodeAt(i)-65)+offset)%26)+65;
-        result+=String.fromCharCode(code);
+      code = (((string.charCodeAt(i)-65)+desplazamiento)%26)+65;
+      result+=String.fromCharCode(code);
       }
       else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-        code = (((string.charCodeAt(i)-97)+offset)%26)+97;
-        result+=String.fromCharCode(code);
+      code = (((string.charCodeAt(i)-97)+desplazamiento)%26)+97;
+      result+=String.fromCharCode(code);
       }
       else if (string.charCodeAt(i)===32){
-        code=32;
-        result+=String.fromCharCode(code);
+      code=32;
+      result+=String.fromCharCode(code);
       }
     } return result;
+};
+console.log(desplazamiento); //trae select html
+console.log(encode("Abrahamz", 2));
     
-  };
-console.log(encode("Lu", 2));
-
+    
+    
+  // decode = (string, offset)=>{
+  //     let resultDecode = "", code;
+  //       for (let i=0; i<string.length;i++){
+  //         if(string.charCodeAt(i)<=65 && string.charCodeAt(i)>=90){
+  //           code = (((string.charCodeAt(i)-65)+offset)%26)-65;
+  //           resultDecode+=String.fromCharCode(code);
+  //         }
+  //         else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+  //           code = (((string.charCodeAt(i)-97)+offset)%26)-97;
+  //           resultDecode+=String.fromCharCode(code);
+  //         }
+  //         else if (string.charCodeAt(i)===32){
+  //           code=32;
+  //           resultDecode+=String.fromCharCode(code);
+  //         }
+  //       } return resultDecode;       
+  //     };
+  //   console.log(decode("jk", 2));
+    
 export default encode;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,101 @@
+
 import encode from './cipher.js';
 
-console.log(cipher);
+console.log(encode);
 
 document.getElementById ("start").style.display = "none"
 document.getElementById ("myNotes").style.display = "none"
 document.getElementById ("newNote").style.display = "none"
 
-
-
-function misNotas(){
-  let btnMyNotes;
-  btnMyNotes = document.getElementById("btnMyNotes");
-  btnMyNotes.addEventListener("click","goMyNotes");  
-}
-function goMyNotes(){
-  document.getElementById ("start").style.display = "none"
-  document.getElementById ("newNote").style.display = "none"
-  document.getElementById ("myNotes").style.display = "block"
+// Botón menú IR A INICIO
+let inicio = document.getElementById("btnStart");
+inicio.addEventListener("click",goInicio);
+function goInicio() {
+  document.getElementById ("newNote").style.display = "none";
+  document.getElementById ("myNotes").style.display = "none";  
+  document.getElementById ("start").style.display = "block";
 }
 
+// Botón menú IR A NUEVA NOTA
+let nvaNota = document.getElementById("btnNewNote");
+nvaNota.addEventListener("click",goNewNote);
+function goNewNote() {
+  document.getElementById ("start").style.display = "none";
+  document.getElementById ("myNotes").style.display = "none";
+  document.getElementById ("newNote").style.display = "block";
+}
+
+// Botón menú IR A MIS NOTAS
+let misNotas = document.getElementById("btnMyNotes");
+misNotas.addEventListener("click",goMyNotes);
+function goMyNotes() {
+  document.getElementById ("start").style.display = "none";
+  document.getElementById ("newNote").style.display = "none";
+  document.getElementById ("myNotes").style.display = "block";  
+}
+
+document.getElementById("btnCifrar");
+document.getElementById("btnDescifrar");
+document.getElementById("userInput");
+document.getElementById("userOutput");
+document.getElementById("desplazamiento");
+
+
+let msgUser=document.getElementById()
+
+let verTextCifrado=document.getElementById("btnCifrar");
+verTextCifrado.addEventListener("click",textCifrado);
+function textCifrado() {
+  let msgCifrado=document.querySelector(encode).value;
+  document.querySelector(msgCifrado).innerHTML+=userOutput;
+}
+console.log(verTextCifrado); 
+
+
+
+// let userStringIn = document.getElementById("string")
+// let cipherText = document.getElementById("cifrar");
+// cipherText.addEventListener("click",cifrado);
+// function cifrado() {
+//   document.getElementById("encode").innerHTML="result";
+
+// document.getElementById("btnCifrar");
+// document.getElementById("btnDescifrar");
+// document.getElementById("userInput");
+
+
+
+
+// console.log(offset);
+
+// let cifrarTexto = document.getElementById("btnCifrar");
+// cifrarTexto.addEventListener("click",encodeMsg);
+// function encodeMsg() {
+//   document.getElementById ("newNote").style.display = "none";
+//   document.getElementById ("myNotes").style.display = "none";  
+//   document.getElementById ("start").style.display = "block";
+// }
+
+  //  decode = (string, offset)=>{
+  //      let resultDecode = "", code;
+  //        for (let i=0; i<string.length;i++){
+  //          if(string.charCodeAt(i)<=65 && string.charCodeAt(i)>=90){
+  //            code = (((string.charCodeAt(i)-65)+offset)%26)-65;
+  //            resultDecode+=String.fromCharCode(code);
+  //          }
+  //          else if (string.charCodeAt(i)<=97 && string.charCodeAt(i)>=122){
+  //            code = (((string.charCodeAt(i)-97)+offset)%26)-97;
+  //            resultDecode+=String.fromCharCode(code);
+  //          }
+  //          else if (string.charCodeAt(i)===32){
+  //            code=32;
+  //            resultDecode+=String.fromCharCode(code);
+  //          }
+  //        } return resultDecode;       
+  //      };
+  //    console.log(decode("jk",2));
 
   
 
 
+ 
